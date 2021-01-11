85a3ca708739b1472c1e881772da1668f22168a4
impact-eintr <yixingwei4@gmail.com>
2021-01-11 21:57:36 +0800

    first commit

--- A/1046/ans.cpp
+++ A/1046/ans.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
+
 int main()
 {
     int n =0;
     std::cin >> n;
+    int sum =0;
     std::vector<int> dislist(n);
+    std::vector<int> dis(n+1);
+
     for(int i = 0;i < n;i++){
         std::cin >> dislist[i];
+        sum += dislist[i];
+        dis[i+1] = sum;
     }
-    
     //查询
     int m = 0;
     std::cin >> m;
@@ -16,15 +21,11 @@ int main()
         int a,b = 0;
         int disA = 0,disB = 0;//两条路的距离
         std::cin >> a >> b;
+
         int max = a>b?a:b;
         int min = a<b?a:b;
-        for (int j = min-1;j < max-1;j++ ){
-            disA += dislist[j];//从小到大      
-        }
-        for (int j = max-1;j < max+min-1;j++ ){
-            disB += dislist[j%n];//从大到小 
-        }
-        
+        disA = dis[max-1] - dis[min-1];
+        disB = dis[n]-disA;
         int res = disA<disB?disA:disB;
         std::cout << res << std::endl;
     }
